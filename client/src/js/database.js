9c1bc58342c94bcc1c641281104e65b4f09fb4b2
geekcoldhand <horatiousaharris@gmail.com>
2024-07-09 14:29:08 -0400

    pwa deploy test

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,23 +1,23 @@
 import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  await openDB("jate", 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains("jate")) {
         console.log("jate database already exists");
         return;
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore("editor", { keyPath: "id", autoIncrement: true });
       console.log("jate database created");
     },
   });
 
 export const putDb = async (content) => {
-  console.log("putDb");
+  console.log("putDB content", content);
   const editorDB = await openDB("jate", 1);
   // Create a new transaction and specify the database and data privileges.
   const tx = editorDB.transaction("jate", "readwrite");
-  const store = tx.objectStore("contact");
+  const store = tx.objectStore("jate");
   const request = store.add(content);
   const result = await request;
   console.log("DATA SAVED . . .", result);
@@ -28,7 +28,7 @@ export const getDb = async () => {
   // Create a new transaction and specify the database and data privileges.
   const tx = contactDb.transaction("jate", "readonly");
   // Open up the desired object store.
-  const store = tx.objectStore("jate");
+  const store = tx.objectStore("editor");
   const request = store.getAll();
   // Get confirmation of the request.
   const result = await request;
